dccc81d4eacdcd8424709650e3654022fe8a6177
DaneKellogg <dane.smith@kellogg.northwestern.edu>
2022-02-08 03:07:05 +0000

    Try 2

--- kmdb.rb
+++ kmdb.rb
@@ -70,7 +70,8 @@
 # Use `Model.destroy_all` code.
 # TODO!
 
-Model.destroy_all
+Movie.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model
 # TODO!
@@ -78,6 +79,117 @@ Model.destroy_all
 # Insert data into your database that reflects the sample data shown above
 # Do not use hard-coded foreign key IDs.
 # TODO!
+
+movie1 = movie.new
+movie1.title = "Batman Begins"
+movie1.year_released = "2005"
+movie1.rating = "PG-13"
+movie1.person_id = "Christopher Nolan"
+movie1.save
+
+movie2 = movie.new
+movie2.title = "The Dark Knight"
+movie2.year_released = "2008"
+movie2.rating = "PG-13"
+movie2.person_id = "Christopher Nolan"
+movie2.save
+
+movie3 = movie.new
+movie3.title = "The Dark Knight Rises"
+movie3.year_released = "2012"
+movie3.rating = "PG-13"
+movie3.person_id = "Christopher Nolan"
+movie3.save
+
+role1 = role.new
+role1.name = "Bruce Wayne"
+role1.movie_id = "Batman Begins"
+role1.person_id = "Christian Bale"
+role1.save
+
+role2 = role.new
+role2.name = "Bruce Wayne"
+role2.movie_id = "The Dark Knight"
+role2.person_id = "Christian Bale"
+role2.save
+
+role3 = role.new
+role3.name = "Bruce Wayne"
+role3.movie_id = "The Dark Knight Rises"
+role3.person_id = "Christian Bale"
+role3.save
+
+role4 = role.new
+role4.name = "Alfred"
+role4.movie_id = "Batman Begins"
+role4.person_id = "Michael Caine"
+role4.save
+
+role5 = role.new
+role5.name = "Ra's Al Ghul"
+role5.movie_id = "Batman Begins"
+role5.person_id = "Liam Neeson"
+role5.save
+
+role6 = role.new
+role6.name = "Rachel Dawes"
+role6.movie_id = "Batman Begins"
+role6.person_id = "Katie Holmes"
+role6.save
+
+role7 = role.new
+role7.name = "Commissioner Gordan"
+role7.movie_id = "Batman Begins"
+role7.person_id = "Gary Oldman"
+role7.save
+
+role8 = role.new
+role8.name = "Joker"
+role8.movie_id = "The Dark Knight"
+role8.person_id = "Heath Ledger"
+role8.save
+
+role9 = role.new
+role9.name = "Harvey Dent"
+role9.movie_id = "The Dark Knight"
+role9.person_id = "Aaron Eckhart"
+role9.save
+
+role10 = role.new
+role10.name = "Alfred"
+role10.movie_id = "The Dark Knight"
+role10.person_id = "Michael Caine"
+role10.save
+
+role11 = role.new
+role11.name = "Rachel Dawes"
+role11.movie_id = "The Dark Knight"
+role11.person_id = "Maggie Gyllenhaal"
+role11.save
+
+role12 = role.new
+role12.name = "Commissioner Gordan"
+role12.movie_id = "The Dark Knight Rises"
+role12.person_id = "Gary Oldman"
+role12.save
+
+role13 = role.new
+role13.name = "Bane"
+role13.movie_id = "The Dark Knight Rises"
+role13.person_id = "Tom Hardy"
+role13.save
+
+role14 = role.new
+role14.name = "John Blake"
+role14.movie_id = "The Dark Knight Rises"
+role14.person_id = "Joseph Gordon-Levitt"
+role14.save
+
+role15 = role.new
+role15.name = "Selina Kyle"
+role15.movie_id = "The Dark Knight Rises"
+role15.person_id = "Anne Hathaway"
+role15.save
 
 # Prints a header for the movies output
 puts "Movies"
